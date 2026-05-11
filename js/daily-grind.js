ee28cbcd08615eeae8cd7e45e7165d5447902256
tako <takeo2039@gmail.com>
2026-05-10 19:14:24 -0700

    adds days of week retrieval

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,1 +1,32 @@
 alert("hello from daily-grind.js!");
+
+myData = new Date();
+myDay = myDate.getDay();
+
+switch(myDay) {
+   case 0:
+      today = "Sunday";
+      break;
+   case 1:
+      today = "Monday";
+      break;
+   case 2:
+      today = "Tuesday";
+      break;
+   case 3:
+      today = "Wednesday";
+      break;
+   case 4:
+      today = "Thursday";
+      break;
+   case 5:
+      today = "Friday";
+      break;
+   case 6:
+      today = "Saturday";
+      break;
+   default:
+      today "Something went wrong!";
+}
+
+alert(today);

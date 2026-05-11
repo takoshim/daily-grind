cb22cfd439b93c7368ae583a59ff37a1c29ab0da
tako <takeo2039@gmail.com>
2026-05-10 21:06:39 -0700

    adds function coffeeTemplate()

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,5 +1,5 @@
 
-var myData = new Date();
+var myDate = new Date();
 var myDay = myDate.getDay();
 var today = "";
 
@@ -7,16 +7,30 @@ switch(myDay) {
    case 0:
       today = "Sunday";
       coffee = {
-         pic:"";
-         alt:"";
-         desc:"";
-         day:"";
-         color:"";
-         name:"";
+         name:"Bubble Tea",
+         pic:"bubble-tea.jpg",
+         alt:"A pic of a yuymmy bubble tea.",
+         color:"pink",
+         day:"Tuesday",
+         desc: `I like me some Bubble Tea!`
       };
       break;
    default:
-      today "Something went wrong!";
+      today = "Something went wrong!";
 }
 
+console.log(coffee);
 alert(today);
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+function coffeeTemplate(coffee) {
+   let myReturn = "";
+
+
+   myReturn += 
+      `<p>
+			<img src="images/${coffee.pic} alt="${coffee.alt}" id="coffee">
+         <strong>${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong>${coffee.name}</strong>${coffee.desc}
+		</p>`
+}
+

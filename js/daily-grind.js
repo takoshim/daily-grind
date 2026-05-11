04f9c75df607a4702ed20e5b5d60563f7c4ce4c1
tako <takeo2039@gmail.com>
2026-05-10 19:44:30 -0700

    fixes day of Sunday retrieval

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,29 +1,19 @@
-alert("hello from daily-grind.js!");
 
-myData = new Date();
-myDay = myDate.getDay();
+var myData = new Date();
+var myDay = myDate.getDay();
+var today = "";
 
 switch(myDay) {
    case 0:
       today = "Sunday";
-      break;
-   case 1:
-      today = "Monday";
-      break;
-   case 2:
-      today = "Tuesday";
-      break;
-   case 3:
-      today = "Wednesday";
-      break;
-   case 4:
-      today = "Thursday";
-      break;
-   case 5:
-      today = "Friday";
-      break;
-   case 6:
-      today = "Saturday";
+      coffee = {
+         pic:"";
+         alt:"";
+         desc:"";
+         day:"";
+         color:"";
+         name:"";
+      };
       break;
    default:
       today "Something went wrong!";
